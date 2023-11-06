22f7ed87df8c49a8f6b3485daf4f7040c47fcdf4
nothehi <nodehi7813@gmail.com>
2023-11-06 20:02:06 +0330

    feat: add input component

---
+++ src/components/form/TheInput.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface Props {
+    title: string,
+    type: string,
+    placeholder?: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+    <div class="flex flex-col items-start justify-center w-full">
+        <span class="text-sm font-medium mb-1">{{title}}</span>
+        <input class="rounded-lg px-4 py-2 text-sm border border-gray-200 outline-red-500 w-full" :type="type" :placeholder="placeholder">
+    </div>
+</template>
