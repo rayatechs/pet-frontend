92bf71a56554dba2e5487cb94a72c9d12b6f3045
nothehi <nodehi7813@gmail.com>
2023-11-08 20:59:30 +0330

    feat: add modelvalue to the input component

--- src/components/form/TheInput.vue
+++ src/components/form/TheInput.vue
@@ -1,16 +1,34 @@
 <script setup lang="ts">
+import { computed, ref, watch } from 'vue';
 interface Props {
     title: string,
+    name: string,
     type: string,
-    placeholder?: string
+    placeholder?: string,
+    modelValue: string
 }
 
-defineProps<Props>()
+const props = defineProps<Props>()
+const emit = defineEmits(['update:modelValue'])
+
+const value = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value) {
+    emit('update:modelValue', value)
+  }
+})
 </script>
 
 <template>
-    <div class="flex flex-col items-start justify-center w-full">
-        <span class="text-sm font-medium mb-1">{{title}}</span>
-        <input class="rounded-lg px-4 py-2 text-sm border border-gray-200 outline-red-500 w-full" :type="type" :placeholder="placeholder">
+    <div class="flex flex-col items-start justify-center w-full mt-4">
+        <label :for="name" class="text-sm font-medium mb-1">{{title}}</label>
+        <input 
+            :id="name"
+            class="rounded-lg px-4 py-2 text-sm border border-gray-200 outline-red-500 w-full" 
+            :type="type" 
+            :placeholder="placeholder"
+            v-model="value">
     </div>
 </template>
--- src/views/Auth/RegisterView.vue
+++ src/views/Auth/RegisterView.vue
@@ -8,11 +8,11 @@ import TheInput from '@/components/form/TheInput.vue';
             <h1 class="mt-36 font-semibold text-2xl text-white">ساخت حساب کاربری</h1>
         </header>
 
-        <main class="flex flex-col items-center justify-center mt-10 w-full space-y-4 px-20">
-            <the-input title="نام" type="text" placeholder="علی" />
-            <the-input title="پست الکترونیک" type="text" placeholder="ali@gmai.com" />
-            <the-input title="شماره تلفن همراه" type="text" placeholder="09123456789" />
-            <the-input title="رمز عبور" type="password" placeholder="*********" />
+        <main class="flex flex-col items-center justify-center mt-10 w-full px-20">
+            <the-input v-model="form.name" name="name" title="نام" type="text" placeholder="علی" />
+            <the-input v-model="form.email" name="email" title="پست الکترونیک" type="text" placeholder="ali@gmai.com" />
+            <the-input v-model="form.mobile" name="mobile" title="شماره تلفن همراه" type="text" placeholder="09123456789" />
+            <the-input v-model="form.password" name="password" title="رمز عبور" type="password" placeholder="*********" />
             <button class="bg-red-500 rounded-full text-white w-full py-2 hover:bg-red-700">ثبت نام</button>
         </main>
     </div>
