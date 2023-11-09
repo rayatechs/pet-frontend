154df15fa84a6b7cd7704630975ab4cb31dd1360
nothehi <nodehi7813@gmail.com>
2023-11-09 10:20:16 +0330

    feat: add register api to itself form

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,5 +1,15 @@
 import { defineStore } from 'pinia'
 import { ref, watch } from 'vue'
+import { useUserStore } from './user'
+import axios from '@/axios'
+
+export interface RegisterForm {
+    name: string,
+    email?: string,
+    mobile: string,
+    password: string
+}
+
 
 export const useAuthStore = defineStore('auth', () => {
     const token = ref(localStorage.getItem('token'))
@@ -12,13 +22,18 @@ export const useAuthStore = defineStore('auth', () => {
         }
     })
 
-    function singIn(tokenVal: string) {
-        token.value = tokenVal
-    }
+    function register(form: RegisterForm) {
+        return axios.post('/api/auth/register', form)
+            .then((res) => {
+                const user = useUserStore()
+                user.info = res.data.data.user
+                token.value = res.data.data.token
+            })
+    }    
 
     function singOut() {
         token.value = ''
     }
 
-    return { token, singIn, singOut }
+    return { token, register }
 })
--- src/views/Auth/RegisterView.vue
+++ src/views/Auth/RegisterView.vue
@@ -1,5 +1,21 @@
 <script setup lang="ts">
 import TheInput from '@/components/form/TheInput.vue';
+import { useAuthStore, type RegisterForm } from '@/stores/auth';
+import { reactive } from 'vue';
+import { useRouter, RouterLink } from 'vue-router';
+
+const router = useRouter()
+const auth = useAuthStore()
+const form: RegisterForm = reactive({
+    name: '',
+    email: '',
+    mobile: '',
+    password: ''
+})
+
+function send() {
+    auth.register(form).then(() => router.push('/'))
+}
 </script>
 
 <template>
@@ -13,7 +29,15 @@ import TheInput from '@/components/form/TheInput.vue';
             <the-input v-model="form.email" name="email" title="پست الکترونیک" type="text" placeholder="ali@gmai.com" />
             <the-input v-model="form.mobile" name="mobile" title="شماره تلفن همراه" type="text" placeholder="09123456789" />
             <the-input v-model="form.password" name="password" title="رمز عبور" type="password" placeholder="*********" />
-            <button class="bg-red-500 rounded-full text-white w-full py-2 hover:bg-red-700">ثبت نام</button>
+            <button 
+                @click.prevent="send" 
+                class="bg-red-500 rounded-full text-white w-full py-2 hover:bg-red-700 mt-20">
+                ثبت نام
+            </button>
+            <p class="text-gray-400 text-sm mt-2">
+                حساب کاربری دارید؟ 
+                <router-link to="/login" class="text-gray-600">وارد شوید</router-link>
+            </p>
         </main>
     </div>
 </template>
