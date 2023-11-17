b0b33a38fa167038e542445be7f6dde68c6c0068
nothehi <nodehi7813@gmail.com>
2023-11-17 09:02:10 +0330

    refactor: increase readablity of back home button

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -3,16 +3,17 @@ import { IconChevronLeft } from '@tabler/icons-vue';
 import { useRouter } from 'vue-router';
 
 const emit = defineEmits(['onBack'])
+
 const router = useRouter()
 
 function back() {
-    router.push('/'); 
     emit('onBack')
+    router.push('/'); 
 }
 </script>
 
 <template>
-    <button @click="back" class="absolute text-white top-4 left-[20%] rounded-full border border-white z-10">
-        <Icon-chevron-left class="w-8 h-8 pr-0.5" />
+    <button @click="back" class="mx-1 text-white border border-white rounded-full">
+        <Icon-chevron-left class="w-8 h-8 p-1" />
     </button>
 </template>
