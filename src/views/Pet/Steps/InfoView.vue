affd9bb7eac2360dfe4c1bc7fefdc1740d139cf7
nothehi <nodehi7813@gmail.com>
2023-11-28 21:10:09 +0330

    fix: pet creation functionality and api

--- src/views/Pet/Steps/InfoView.vue
+++ src/views/Pet/Steps/InfoView.vue
@@ -1,19 +1,42 @@
 <script setup lang="ts">
-import { usePetStore } from '@/stores/pet';
-import BaseInput from '@/components/form/BaseInput.vue';
-import BaseImageUploader from '@/components/form/BaseImageUploader.vue'
+import { usePetStore } from '@/stores/pet'
+import { useBreedStore } from '@/stores/breed'
+import BaseInput from '@/components/form/BaseInput.vue'
+import BaseDatePicker from '@/components/form/BaseDatePicker.vue'
+import BaseSelect from '@/components/form/BaseSelect.vue'
+import { computed, type ComputedRef, onMounted } from 'vue'
 
 const pet = usePetStore()
+const breed = useBreedStore()
+
+onMounted(() => {
+  breed.getAll()
+})
+
+const options: ComputedRef = computed(() => {
+  if (pet.type == 'dog') {
+    return breed.dogs.map((breed) => {
+      return { key: breed.id, value: breed.name }
+    })
+  } else {
+    return breed.cats.map((breed) => {
+      return { key: breed.id, value: breed.name }
+    })
+  }
+})
 </script>
 
 <template>
-    <div class="flex flex-col items-center w-full">
-        <base-image-uploader class="-mt-10" />
-
-        <div class="flex flex-col items-center justify-between w-full mt-10">
-            <base-input v-model="pet.form.name" name="name" title="اسم" type="text" />
-            <base-input v-model="pet.form.breed" name="breed" title="نژاد" type="text" />
-            <base-input v-model="pet.form.birthdate" name="birthdate" title="تاریخ تولد" type="date" />
-        </div>
+  <div class="flex flex-col items-center w-full">
+    <div class="flex flex-col items-center justify-between w-full mt-4">
+      <base-input v-model="pet.form.name" name="name" title="اسم" type="text" />
+      <base-date-picker
+        v-model="pet.form.birthdate"
+        name="birthdate"
+        title="تاریخ تولد"
+        type="date"
+      />
+      <base-select v-model="pet.form.breed_id" :options="options" name="breed" title="نژاد" />
     </div>
+  </div>
 </template>
