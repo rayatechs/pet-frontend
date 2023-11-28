3ed737ae70709df57d5cfb07a67ce711b7d6095a
nothehi <nodehi7813@gmail.com>
2023-11-28 21:12:38 +0330

    fix: right to left alignment

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-import { IconChevronLeft } from '@tabler/icons-vue';
-import { useRouter } from 'vue-router';
+import { IconChevronRight } from '@tabler/icons-vue'
+import { useRouter } from 'vue-router'
 
 const emit = defineEmits(['onBack'])
 
 const router = useRouter()
 
 function back() {
-    emit('onBack')
-    router.push('/'); 
+  emit('onBack')
+  router.push('/home')
 }
 </script>
 
 <template>
-    <button @click="back" class="mx-1 text-white border border-white rounded-full">
-        <Icon-chevron-left class="w-8 h-8 p-1" />
-    </button>
+  <button @click="back" class="mx-1 text-white border border-white rounded-full">
+    <Icon-chevron-right class="w-8 h-8 p-1" />
+  </button>
 </template>
