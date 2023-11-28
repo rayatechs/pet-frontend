afdbf757011afcf3e780c3934fec75c9053543ec
nothehi <nodehi7813@gmail.com>
2023-11-28 21:10:09 +0330

    fix: pet creation functionality and api

--- src/stores/pet.ts
+++ src/stores/pet.ts
@@ -1,30 +1,63 @@
 import { defineStore } from 'pinia'
-import { reactive } from 'vue'
+import { reactive, ref, type Ref } from 'vue'
+import axios from '@/axios'
 
-interface PetForm {
-    name: string,
-    image: File,
-    birthdate: string,
-    sex: string,
-    breed: string,
-    type: string,
+export interface Pet {
+  id: number
+  name: string
+  sex: string
+  birthdate: string
+  created_at: string
+  breed: {
+    id: number
+    parent_name: string
+    name: string
+  }
+  avatar: string
+}
+
+interface Form {
+  id: number
+  name: string
+  birthdate: string
+  sex: string
+  breed_id: number
 }
 
 export const usePetStore = defineStore('pet', () => {
-  const initial: PetForm = {
+  const all: Ref<Pet[]> = ref([])
+  let image: File = new File([''], '')
+  const type = ref('')
+  const initial: Form = {
+    id: 0,
     name: '',
-    image: new File([""], ""),
     birthdate: '',
     sex: '',
-    breed: '',
-    type: '',
+    breed_id: 0
   }
-
-  const form: PetForm = reactive({...initial})
+  const form: Form = reactive({ ...initial })
 
   function resetForm() {
     Object.assign(form, initial)
+    type.value = ''
+    image = new File([''], '')
   }
 
-  return { form, resetForm }
+  function store() {
+    return axios.post('/api/pet', form)
+  }
+
+  function getAll() {
+    return axios.get('/api/pet').then((res) => (all.value = res.data.data))
+  }
+
+  function get(id: number) {
+    return axios.get(`/api/pet/${id}`)
+  }
+
+  function storeAvatar(petId: number) {
+    return axios.post(`/api/pet/${petId}/upload-avatar`, { avatar: image })
+  }
+
+  return { all, image, type, form, resetForm, store, getAll, get, storeAvatar }
 })
