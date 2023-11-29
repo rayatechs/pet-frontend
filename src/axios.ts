d9ab2b10357d4d3c66cd95be138f6d2cdcc001a8
nothehi <nodehi7813@gmail.com>
2023-11-29 19:59:41 +0330

    fix: add production url for apis

--- src/axios.ts
+++ src/axios.ts
@@ -4,48 +4,47 @@ import { useValidationStore } from '@/stores/validation'
 import { useToasterStore } from '@/stores/toaster'
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:8000',
-    timeout: 1000,
-    // headers: { 'X-Custom-Header': 'foobar' },
+  baseURL: 'https://ropet.ir',
+  headers: { 'Content-Type': 'application/json', Accept: 'application/json' }
 })
 
 export function setupInterceptors() {
   const toaster = useToasterStore()
-    // Add a request interceptor
+  // Add a request interceptor
   instance.interceptors.request.use(
     (config: any) => {
-        const { token } = useAuthStore()
-        const specificString = 'login'
-        // Do something before request is sent
-        // config.withCredentials = true
-        if (!config.url.includes(specificString)) {
-            config.headers.Authorization = `Bearer ${token}`
-        }
+      const { token } = useAuthStore()
+      const specificString = 'login'
+      // Do something before request is sent
+      // config.withCredentials = true
+      if (!config.url.includes(specificString)) {
+        config.headers.Authorization = `Bearer ${token}`
+      }
 
-        return config
+      return config
     },
     (error: any) => {
       toaster.show('لطفا اینترنت خود را بررسی کنید!', 'warning')
-        // Do something with request error
-        Promise.reject(error)
+      // Do something with request error
+      Promise.reject(error)
     }
   )
 
-    // Add a response interceptor
-    instance.interceptors.response.use(
-        (response: any) => {
-            return response
-        }, 
-        (error: any) => {
-            if (error.response?.status == 422) {
-                const validation = useValidationStore()
-                validation.errors = error.response.data.errors
+  // Add a response interceptor
+  instance.interceptors.response.use(
+    (response: any) => {
+      return response
+    },
+    (error: any) => {
+      if (error.response?.status == 422) {
+        const validation = useValidationStore()
+        validation.errors = error.response.data.errors
         toaster.show('لطفا تمامی فیلد ها را پر کنید!', 'warning')
       } else if (error.response?.status == 500) {
         toaster.show('مشکلی پیش آمده! مجددا تلاش کنید.', 'error')
-            }
-            
-            return Promise.reject(error.response)
+      }
+
+      return Promise.reject(error.response)
     }
   )
 }
